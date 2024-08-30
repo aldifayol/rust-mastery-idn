bc165eecf7813600671f31461b59a1a21e198a07
Aldi <aldifayol@gmail.com>
2024-08-31 06:18:04 +0700

    initial commit rust-mastery-idn

---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/aldi/Developer/RUST-APP/rust-mastery/hello_cargo/target/debug/hello_cargo: /Users/aldi/Developer/RUST-APP/rust-mastery/hello_cargo/src/main.rs
---
+++ main.rs
@@ -0,0 +1,3 @@
+fn main() {
+  println!("I feel like being Rusty!");
+}
